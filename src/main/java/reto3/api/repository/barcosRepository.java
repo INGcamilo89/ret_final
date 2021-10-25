ced16583b283ca8cf616d9e4f519e8aedde16508
camilo munoz <camim8919@gmail.com>
2021-10-25 18:11:35 -0500

    creacion de reto 4

--- src/main/java/reto3/api/repository/barcosRepository.java
+++ src/main/java/reto3/api/repository/barcosRepository.java
@@ -3,21 +3,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import reto3.api.model.barcos;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class barcosRepository{
         @Autowired
         barcosCrudRepository BarcosCrudRepository;
-
+        //obtiene lista de barcos
         public List<barcos> obtenerBarcos() {
 
             return (List<barcos>) BarcosCrudRepository.findAll();
         }
-
-        public barcos crearBarcos(barcos Barcos) {
+        //crea lista de barcos
+        public barcos crearBarcos(barcos Barcos)
+        {
         return BarcosCrudRepository.save(Barcos);
-    }
-
-
+    }   //actualiza un barco con id especifico
+        public Optional<barcos> getBarcos(int id){
+            return BarcosCrudRepository.findById(id);
+        }
+        //elimina lista de barcos
+        public void eliminarBarcos(barcos Barcos){
+            BarcosCrudRepository.delete(Barcos);
+        }
 
 }
